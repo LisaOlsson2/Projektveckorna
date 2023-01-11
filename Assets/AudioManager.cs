182a9f0dc0b68501d79b878d9c213655e92628dc
Mickman321 <97745994+Mickman321@users.noreply.github.com>
2023-01-11 15:26:09 +0100

    sound effects (footsteps)

---
+++ Assets/AudioManager.cs
@@ -0,0 +1,21 @@
+using unityEngine.Audio;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+
+
+    public Sound[] sounds;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
