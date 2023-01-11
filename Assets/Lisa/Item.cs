a3b844c2d6aa829b6de1d52b6af8751c2717509d
Lisa Olsson <97893739+LisaOlsson2@users.noreply.github.com>
2023-01-11 13:43:37 +0100

    sprint and stamina

--- Assets/Lisa/Item.cs
+++ Assets/Lisa/Item.cs
@@ -5,13 +5,9 @@ using UnityEngine.UI;
 
 public class Item : MonoBehaviour
 {
+    ItKnows reference;
+
     bool interactable;
-
-    [SerializeField]
-    Transform canvas;
-
-    [SerializeField]
-    GameObject emptyKinda;
 
     GameObject item;
 
@@ -19,11 +15,16 @@ public class Item : MonoBehaviour
 
     static List<GameObject> inventory = new List<GameObject>();
 
+    void Start()
+    {
+        reference = FindObjectOfType<ItKnows>();
+    }
+
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Z) && interactable)
         {
-            item = Instantiate(emptyKinda, canvas);
+            item = Instantiate(reference.emptyKinda, reference.canvas);
             item.GetComponent<Image>().sprite = GetComponent<SpriteRenderer>().sprite;
             item.GetComponent<RectTransform>().anchoredPosition += new Vector2(distance * inventory.Count, 0);
             inventory.Add(item);
--- Assets/Lisa/TempMovement.cs
+++ Assets/Lisa/TempMovement.cs
@@ -1,7 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
+// Lisa
 public class TempMovement : MonoBehaviour
 {
     [SerializeField]
@@ -10,7 +12,10 @@ public class TempMovement : MonoBehaviour
     readonly float dashDuration = 0.3f;
     readonly float dashForce = 500;
     readonly float jumpForce = 250;
-    readonly float speed = 5;
+    readonly float speed = 2;
+    readonly float stamina = 4;
+    readonly float sprintInputDelay = 0.5f;
+    readonly int sprint = 7;
 
     readonly float rightBorder = 10;
     readonly float leftBorder = -20;
@@ -19,25 +24,72 @@ public class TempMovement : MonoBehaviour
 
     bool grounded;
     float camDistanceX;
+    float staminaTimer;
+    int speedMultiplier = 1;
+    float sprintTimer;
 
     Rigidbody2D rb;
+
+    [SerializeField]
+    Slider slider;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        slider.maxValue = stamina;
     }
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        print(speedMultiplier);
+
+        slider.value = Mathf.Abs(staminaTimer);
+
+        if (speedMultiplier == sprint)
         {
-            if (Input.GetKey(KeyCode.D))
+            staminaTimer -= Time.deltaTime;
+
+            if (staminaTimer < 0)
             {
-                StartCoroutine(Dash(1));
+                slider.fillRect.GetComponent<Image>().color = Color.red;
+                sprintTimer = 0;
+                speedMultiplier = 1;
+                staminaTimer = -4;
             }
-            else if (Input.GetKey(KeyCode.A))
+        }
+        else if (staminaTimer < stamina)
+        {
+            staminaTimer += Time.deltaTime;
+            if (staminaTimer > 0)
             {
-                StartCoroutine(Dash(-1));
+                slider.fillRect.GetComponent<Image>().color = Color.green;
+            }
+        }
+
+        if (staminaTimer > 0)
+        {
+            if (speedMultiplier == 2 && sprintTimer < sprintInputDelay)
+            {
+                sprintTimer += Time.deltaTime;
+                if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.D))
+                {
+                    speedMultiplier = sprint;
+                }
+            }
+            else
+            {
+                if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.A))
+                {
+                    speedMultiplier = 2;
+                }
+                if (Input.GetKeyUp(KeyCode.D) || Input.GetKeyUp(KeyCode.A))
+                {
+                    if (sprintTimer >= sprintInputDelay || speedMultiplier == sprint)
+                    {
+                        sprintTimer = 0;
+                    }
+                    speedMultiplier = 1;
+                }
             }
         }
 
@@ -46,13 +98,38 @@ public class TempMovement : MonoBehaviour
             rb.AddForce(transform.up * jumpForce);
         }
 
-        if (Input.GetKey(KeyCode.D) && transform.position.x < rightBorder)
+        if (transform.position.x < rightBorder)
         {
-            transform.position += new Vector3(speed, 0, 0) * Time.deltaTime;
+            if (Input.GetKey(KeyCode.D))
+            {
+                transform.position += new Vector3(speed * speedMultiplier, 0, 0) * Time.deltaTime;
+
+                if (Input.GetKeyDown(KeyCode.Space) && staminaTimer > 0)
+                {
+                    StartCoroutine(Dash(1));
+                }
+            }
         }
-        if (Input.GetKey(KeyCode.A) && transform.position.x > leftBorder)
+        else
         {
-            transform.position += new Vector3(-speed, 0, 0) * Time.deltaTime;
+            rb.velocity = new Vector3(0, rb.velocity.y, 0);
+        }
+
+        if (transform.position.x > leftBorder)
+        {
+            if (Input.GetKey(KeyCode.A))
+            {
+                transform.position += new Vector3(-speed * speedMultiplier, 0, 0) * Time.deltaTime;
+
+                if (Input.GetKeyDown(KeyCode.Space) && staminaTimer > 0)
+                {
+                    StartCoroutine(Dash(-1));
+                }
+            }
+        }
+        else
+        {
+            rb.velocity = new Vector3(0, rb.velocity.y, 0);
         }
 
         camDistanceX = transform.position.x - cam.transform.position.x;
@@ -65,6 +142,7 @@ public class TempMovement : MonoBehaviour
 
     IEnumerator Dash(int direction)
     {
+        staminaTimer -= 1;
         rb.gravityScale = 0;
         rb.velocity = Vector3.zero;
         rb.AddForce(new Vector3(dashForce * direction, 0, 0));
