2d2d2d2446e6f673888a7863aad43719b907361d
ElliotLBS <98454374+ElliotLBS@users.noreply.github.com>
2023-01-12 14:57:24 +0100

    Enemy2

---
+++ Assets/Elliot/Scripts/SpecifikSpawner.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpecifikSpawner : MonoBehaviour
+{
+
+    [SerializeField]
+    private GameObject GrosuggaSpawner;
+    float timer;
+    float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Den här gör så att unity enemyn spawnar en ny av sig själv på en timer
+        timer += Time.deltaTime;
+        if (timer > 2)
+        {
+            Instantiate(GrosuggaSpawner, transform.position, GrosuggaSpawner.transform.rotation);
+            timer = 0;
+        }
+
+    }
+}
+
+
+   
