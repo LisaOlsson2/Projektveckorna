0117e6068ab3004ac1d422244f47cbd0dc17fe53
Lisa Olsson <97893739+LisaOlsson2@users.noreply.github.com>
2023-01-31 17:22:29 +0100

    blåbär

--- Assets/Lisa/Scripts/Water.cs
+++ Assets/Lisa/Scripts/Water.cs
@@ -5,11 +5,30 @@ using UnityEngine;
 // Lisa
 public class Water : Interactive
 {
+    [SerializeField]
+    Sprite filled;
+
+    [SerializeField]
+    Sprite[] sprites;
+
+    int current;
+
     private void Update()
     {
         if (Input.GetKeyDown(inventory.pickUp) && interactable && inventory.square.gameObject.activeSelf && inventory.CurrentSprite() == inventory.empty)
         {
-            inventory.ChangeSprite(inventory.FindSprite(inventory.empty), inventory.dirty);
+            inventory.ChangeSprite(inventory.FindSprite(inventory.empty), filled);
+
+            if (sprites.Length > 0)
+            {
+                spriteRenderer.sprite = sprites[current];
+                current++;
+
+                if (current == sprites.Length)
+                {
+                    Destroy(this);
+                }
+            }
         }
     }
 }
